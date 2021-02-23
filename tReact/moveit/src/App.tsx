ef10b61d3d3f9783973dc75bf8f2ab58b03c59ee
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-02-23 01:05:30 -0300

    first commit

---
+++ tReact/moveit/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <Button color='red'>
+        Botão1 
+        </Button>
+      <Button color='blue'>Botão2</Button>
+      <Button color='green'>Botão3</Button>
+    </div>
+  );
+}
+
+export default App;
