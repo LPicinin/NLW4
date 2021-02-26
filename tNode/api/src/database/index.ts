cc51f0074f292fdcb8a049092eb0d20c2a0121b0
LPicinin <luishpj@outlook.com.br>
2021-02-26 00:12:41 -0300

    trilha node aula 3 até 19:27, já tá tarde :(

--- tNode/api/src/database/index.ts
+++ tNode/api/src/database/index.ts
@@ -2,3 +2,6 @@ import {createConnection} from 'typeorm'
 
 
 createConnection()
+
+//https://nextlevelweek.com/episodios/node/3/edicao/4
+//https://youtu.be/jGHvrFiA0vQ?t=1167
--- tNode/api/src/routes.ts
+++ tNode/api/src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import { SurveysController } from './controllers/SurveysController'
 import { UserController } from './controllers/UserController'
 
 /**
@@ -13,8 +14,10 @@ const router = Router()
 
 
 const userController = new UserController()
+const surveyController = new SurveysController()
 
 router.post("/users", userController.create)
+router.post("/surveys", surveyController.create)
 
 
 export {router}
